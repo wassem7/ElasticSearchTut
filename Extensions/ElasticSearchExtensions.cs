4bf812fc322947f473f2fb00e84e5133b0f63d6f
wassem7 <wassemdarkwa@gmail.com>
2024-01-25 15:06:20 +0000

    Elastic search client configuration

---
+++ Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,33 @@
+﻿using ElasticSearch.Models;
+using Nest;
+
+namespace ElasticSearch.Extensions;
+
+public static class ElasticSearchExtensions
+{
+    public static void AddElasticSearch(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var url = configuration["ELKConfiguration:Uri"];
+        var defaultIndex = configuration["ELKConfiguration:index"];
+        var settings = new ConnectionSettings(new Uri(url)).PrettyJson().DefaultIndex(defaultIndex);
+        AddDefaultMappings(settings);
+        var client = new ElasticClient(settings);
+        services.AddSingleton<IElasticClient>(client);
+        AddDefaultIndex(client, defaultIndex);
+    }
+
+    private static void AddDefaultMappings(ConnectionSettings settings)
+    {
+        settings.DefaultMappingFor<Item>(
+            i => i.Ignore(p => p.Price).Ignore(p => p.Id).Ignore(p => p.Quantity)
+        );
+    }
+
+    private static void AddDefaultIndex(IElasticClient client, string indexName)
+    {
+        client.Indices.Create(indexName, i => i.Map<Item>(a => a.AutoMap()));
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElasticSearch.Models;
+
+public class Item
+{
+    public Guid Id { get; set; }
+
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+
+    public int Quantity { get; set; }
+
+    public decimal Price { get; set; }
+}
